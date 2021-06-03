75ec8bff1779cbb9102e6da9c2cace0302c3c406
FredLavoie <flavoie3@gmail.com>
2021-06-03 07:35:48 -0400

    Add loading to Search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
@@ -47,6 +48,11 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'space-around'
   },
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '10%',
+  },
 });
 
 
@@ -55,18 +61,21 @@ function Search() {
   const [checkedWorkout, setCheckedWorkout] = useState(true);
   const [checkedRecord, setCheckedRecord] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleClear() {
     setSearchQuery('');
-    setSearchResults([]);
+    setSearchResults(null);
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     const results = await fetchSearchResults(checkedWorkout, checkedRecord, searchQuery);
     const sortedResults = results.sort((a, b) => b.date > a.date);
     setSearchResults(sortedResults);
+    setIsLoading(false);
   }
 
   return (
@@ -129,8 +138,9 @@ function Search() {
           </Button>
         </div>
       </form>
-      {searchResults.length > 0 ? <Typography color='primary'>( Number of results found: {searchResults.length} )</Typography> : ''}
-      {searchResults.length > 0 ? <SearchResultCard content={searchResults} /> : <div></div>}
+      {isLoading && <div className={classes.loading}><CircularProgress /></div>}
+      {searchResults && searchResults.length > 0 ? <Typography color='primary'>( Number of results found: {searchResults.length} )</Typography> : ''}
+      {searchResults && searchResults.length > 0 ? <SearchResultCard content={searchResults} /> : <div></div>}
     </Grid>
   );
 }
