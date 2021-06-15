272b2a7706de05043a1992f1d837fa6647c10dcc
FredLavoie <flavoie3@gmail.com>
2021-06-15 08:25:00 -0400

    Prepare config for production
    - create a development specific settings file
    - import dev settings if in development environment
    - change admin consol url

--- server/config/settings.py
+++ server/config/settings.py
@@ -25,12 +25,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fdrao0y^p+6eq5+hk^$e8nlsnf*a9zyl6%z#!u+fsak)=uh#be'
+SECRET_KEY = os.getenv('DJANGO_SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('HOST')]
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 2592000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 
 # Application definition
@@ -167,3 +177,6 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',  # React frontend
     'http://localhost:8000',  # Django API
 )
+
+if os.environ.get('DJANGO_DEVELOPMENT'):
+    from .settings_dev import *
--- server/config/urls.py
+++ server/config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('config-panel/', admin.site.urls),
     path('api/', include('workouts.urls')),
     path('api/', include('records.urls')),
     path('api/', include('accounts.urls')),
