234a997373069ef5883b814bd71e9510f7b0e4a9
FredLavoie <flavoie3@gmail.com>
2021-06-20 08:11:54 -0400

    Revise corse whitelist

--- server/config/settings.py
+++ server/config/settings.py
@@ -182,7 +182,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    'workout-tracker.xyz',
+    'http://workout-tracker.xyz',
     'http://localhost:3000',  # React frontend
     'http://localhost:8000',  # Django API
 )
