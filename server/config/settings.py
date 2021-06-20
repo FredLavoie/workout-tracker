f8cc5f24cf69568939bd175c3f8b73c63bc5b548
FredLavoie <flavoie3@gmail.com>
2021-06-20 07:30:04 -0400

    Add STATIC_ROOT variable to settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -163,6 +163,7 @@ TIME_INPUT_FORMATS = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
