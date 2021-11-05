0378b96efffd6190089a4c21ca1644a298d0d9d1
FredLavoie <flavoie3@gmail.com>
2021-11-05 10:47:38 -0400

    Fix small linting issue

--- server/workouts/serializers.py
+++ server/workouts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Workout
 
+
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'date', 'time', 'workout_body', 'created_at', 'updated_at')
