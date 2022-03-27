3e3f7ab997bccc637d5b0269b4cc30e46d22d795
FredLavoie <flavoie3@gmail.com>
2022-03-26 21:36:46 -0400

    Revise time for new workouts to use current hours instead of the next hour

--- client/src/utils/convertTime.js
+++ client/src/utils/convertTime.js
@@ -1,7 +1,6 @@
+// Take a time array and converts it into a time with the format 00:00
 export function convertTime(timeArr) {
-  if (timeArr[0] === '23') return '23:00';
-
-  const hour = (Number(timeArr[0]) + 1).toString();
+  const hour = Number(timeArr[0]).toString();
   const paddedHour = hour.padStart(2, '0');
   return `${paddedHour}:00`;
 }
