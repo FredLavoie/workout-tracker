d388e2db555fac35d662c18fc4367db4dc56eeb7
FredLavoie <flavoie3@gmail.com>
2022-04-15 21:39:47 -0400

    Add test for validateWorkout utility function + revise validation

--- client/src/utils/validateWorkout.js
+++ client/src/utils/validateWorkout.js
@@ -8,22 +8,26 @@
  * @return {boolean} if the data is valid, return true, else return false
  */
 export function validateWorkout(date, time, body) {
+  // validate date
   if (date === '' || !date.includes('-')) return false;
-  if (time === '' || !time.includes(':')) return false;
-  if (body === '') return false;
-
   const dateArr = date.split('-');
-
   if (dateArr.length !== 3) return false;
   if (dateArr[0].length !== 4 || Number(dateArr[0]) % 1 !== 0) return false;
   if (dateArr[1].length !== 2 || Number(dateArr[1]) % 1 !== 0) return false;
   if (dateArr[2].length !== 2 || Number(dateArr[2]) % 1 !== 0) return false;
+  if (dateArr[1] > 12) return false;
+  if (dateArr[2] > 31) return false;
 
+  // validate time
+  if (time === '' || !time.includes(':')) return false;
   const timeArr = time.split(':');
-
   if (timeArr.length !== 2) return false;
   if (timeArr[0].length !== 2 || Number(timeArr[0]) < 0 || Number(timeArr[0]) > 23 || Number(timeArr[0]) % 1 !== 0) return false;
   if (timeArr[1].length !== 2 || Number(timeArr[1]) < 0 || Number(timeArr[1]) > 59 || Number(timeArr[1]) % 1 !== 0) return false;
+
+  // validate body
+  if (body === '') return false;
+  if (typeof body === 'number') return false;
 
   return true;
 }
