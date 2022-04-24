c6c444ce341bf8b48552751e96102972c9bdf3e3
FredLavoie <flavoie3@gmail.com>
2022-04-24 10:54:00 -0400

    Refactor Record component with sx prop for styling

--- client/src/views/Record.jsx
+++ client/src/views/Record.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import {
+  Box,
   Button,
   ButtonGroup,
   CircularProgress,
@@ -16,40 +17,12 @@ import {
   Typography
 } from '@mui/material';
 
-import makeStyles from '@mui/styles/makeStyles';
-
 import MuiAlert from '@mui/material/Alert';
 
 import { fetchRecord, updateRecord, postRecord, deleteRecord } from '../services/fetchData';
 import { validateRecord } from '../utils';
 import { ServerError } from '../components/ServerError';
 import { recordList } from '../lib/recordList';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: 'calc(100vh - 96px)',
-    marginBottom: 32,
-  },
-  formSize: {
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '60%',
-    },
-  },
-  formContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '90%',
-    margin: 'auto',
-    [theme.breakpoints.up('sm')]: {
-      width: '50%',
-      margin: 'auto',
-    },
-  },
-  elementMargin: {
-    marginTop: 16,
-  },
-}));
 
 
 // eslint-disable-next-line prefer-arrow-callback
@@ -59,7 +32,6 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 
 
 export function Record() {
-  const classes = useStyles();
   const location = useLocation();
   const history = useHistory();
 
@@ -167,98 +139,117 @@ export function Record() {
       container
       direction='column'
       alignItems='center'
-      className={classes.root}
+      sx={style.root}
     >
       <Typography variant='h4' gutterBottom>
         Personal Record
       </Typography>
       {error && <ServerError errorMessage={error} />}
       {isLoading && <CircularProgress />}
-      {!error && !isLoading && <Grid className={classes.formSize}>
-        <form noValidate onSubmit={handleSubmit} className={classes.formContainer}>
-          <Typography className={classes.elementMargin}>
-            Date
-          </Typography>
-          <TextField
-            onChange={(e) => setSelectedDate(e.target.value)}
-            margin='normal'
-            id='date-picker'
-            value={selectedDate}
-          />
-          <Typography className={classes.elementMargin}>
-            Event Type
-          </Typography>
-          <RadioGroup
-            row
-            name="recordType"
-            value={recordType}
-            className={classes.elementMargin}
-            onChange={(e) => setRecordType(e.target.value)}
-          >
-            <FormControlLabel
-              value="strength"
-              control={<Radio color="primary" />}
-              label="Strength"
+      {!error && !isLoading &&
+        <Grid sx={style.formSize}>
+          <Box className='FORM-CONTAINER' component='form' noValidate onSubmit={handleSubmit} sx={style.formContainer}>
+            <Typography sx={style.elementMargin}>
+              Date
+            </Typography>
+            <TextField
+              onChange={(e) => setSelectedDate(e.target.value)}
+              margin='normal'
+              id='date-picker'
+              value={selectedDate}
             />
-            <FormControlLabel
-              value="endurance"
-              control={<Radio color="primary" />}
-              label="Endurance"
+            <Typography sx={style.elementMargin}>
+              Event Type
+            </Typography>
+            <RadioGroup
+              row
+              name="recordType"
+              value={recordType}
+              sx={style.elementMargin}
+              onChange={(e) => setRecordType(e.target.value)}
+            >
+              <FormControlLabel
+                value="strength"
+                control={<Radio color="primary" />}
+                label="Strength"
+              />
+              <FormControlLabel
+                value="endurance"
+                control={<Radio color="primary" />}
+                label="Endurance"
+              />
+              <FormControlLabel
+                value="wod"
+                control={<Radio color="primary" />}
+                label="WOD"
+              />
+            </RadioGroup>
+            <Typography sx={style.elementMargin}>
+              Event
+            </Typography>
+            <Select
+              onChange={(e) => setRecordEvent(e.target.value)}
+              sx={style.elementMargin}
+              id='record-event'
+              value={recordEvent}
+            >
+              {recordList[recordType].map((ea, index) => (
+                <MenuItem key={index} value={ea}>{ea}</MenuItem>
+              ))}
+            </Select>
+            <Typography sx={style.elementMargin}>
+              Score
+            </Typography>
+            <TextField
+              onChange={(e) => setRecordScore(e.target.value)}
+              sx={style.elementMargin}
+              id='record-score'
+              value={recordScore}
             />
-            <FormControlLabel
-              value="wod"
-              control={<Radio color="primary" />}
-              label="WOD"
-            />
-          </RadioGroup>
-          <Typography className={classes.elementMargin}>
-            Event
-          </Typography>
-          <Select
-            onChange={(e) => setRecordEvent(e.target.value)}
-            className={classes.elementMargin}
-            id='record-event'
-            value={recordEvent}
-          >
-            {recordList[recordType].map((ea, index) => (
-              <MenuItem key={index} value={ea}>{ea}</MenuItem>
-            ))}
-          </Select>
-          <Typography className={classes.elementMargin}>
-            Score
-          </Typography>
-          <TextField
-            onChange={(e) => setRecordScore(e.target.value)}
-            className={classes.elementMargin}
-            id='record-score'
-            value={recordScore}
-          />
-          <Button
-            fullWidth
-            type={'submit'}
-            className={classes.elementMargin}
-            color='primary'
-            variant='contained'
-            key={`${!selectedDate || !recordEvent || !recordScore ? true : false}`}
-            disabled={!selectedDate || !recordEvent || !recordScore ? true : false}
-          >
-            Save
-          </Button>
-          {
-            newOrEdit === 1
-              ?
-              <Button onClick={handleCancel} className={classes.elementMargin} variant='outlined'>Cancel</Button>
-              :
-              <ButtonGroup fullWidth className={classes.elementMargin}>
-                <Button onClick={handleCancel}>Go Back</Button>
-                <Button onClick={handleDelete}>Delete</Button>
-              </ButtonGroup>
-          }
-        </form>
-      </Grid>}
+            <Button
+              fullWidth
+              type={'submit'}
+              sx={style.elementMargin}
+              color='primary'
+              variant='contained'
+              key={`${!selectedDate || !recordEvent || !recordScore ? true : false}`}
+              disabled={!selectedDate || !recordEvent || !recordScore ? true : false}
+            >
+              Save
+            </Button>
+            {
+              newOrEdit === 1
+                ?
+                <Button onClick={handleCancel} sx={style.elementMargin} variant='outlined'>Cancel</Button>
+                :
+                <ButtonGroup fullWidth sx={style.elementMargin}>
+                  <Button onClick={handleCancel}>Go Back</Button>
+                  <Button onClick={handleDelete}>Delete</Button>
+                </ButtonGroup>
+            }
+          </Box>
+        </Grid>}
       <Snackbar open={open} autoHideDuration={4000} onClose={handleClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
         {alertMessage && <Alert severity={alertMessage.severity}>{alertMessage.message}</Alert>}
       </Snackbar>
     </Grid>
   );
 }
+
+const style = {
+  root: {
+    minHeight: 'calc(100vh - 96px)',
+    marginBottom: '32px',
+  },
+  formSize: {
+    width: { sm: '65%', xs: '90%' },
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+  },
+  elementMargin: {
+    marginTop: '16px',
+  },
+};
