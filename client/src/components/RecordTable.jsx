cdbf273fba470dadb1b51c71b0ae7f9e19bb31a6
FredLavoie <flavoie3@gmail.com>
2022-04-23 23:10:07 -0400

    Update RecordTable with sx prop and named export

--- client/src/components/RecordTable.jsx
+++ client/src/components/RecordTable.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import Typography from '@mui/material/Typography';
-import CardContent from '@mui/material/CardContent';
-import makeStyles from '@mui/styles/makeStyles';
+import {
+  Box,
+  CardContent,
+  Typography,
+} from '@mui/material';
 
 import { recordList } from '../lib/recordList';
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    padding: '0 32px 32px 32px',
-  },
-  individualContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    cursor: 'pointer',
-  },
-  eventContainer: {
-    marginBottom: 8,
-    display: 'flex',
-    justifyContent: 'space-between',
-    cursor: 'pointer',
-  },
-  score: {
-    backgroundColor: theme.palette.secondary.main,
-    padding: '2px 4px',
-    borderRadius: 6,
-    margin: '2px 0',
-  },
-  date: {
-    margin: '2px 0',
-  },
-}));
 
-function RecordTable({ type, records }) {
-  const classes = useStyles();
+export function RecordTable({ type, records }) {
   const history = useHistory();
 
   const sortedRecords = records.sort((a, b) => b.date > a.date);
@@ -46,29 +22,45 @@ function RecordTable({ type, records }) {
   }
 
   return (
-    <CardContent className={classes.content}>
+    <CardContent sx={style.content}>
       {filteredEventList.map((event, index) => (
-        <div key={index} id={event} className={classes.eventContainer} onClick={(e) => handleClickActive(e.currentTarget)}>
+        <Box key={index} id={event} sx={style.eventContainer} onClick={(e) => handleClickActive(e.currentTarget)}>
           <Typography variant={'subtitle1'} >
             {event}
           </Typography>
 
           {[sortedRecords.find((ea) => ea.event === event)].map((ea) => {
             return (
-              <div
+              <Box
                 key={ea.id}
                 id={ea.event}
               >
-                <Typography variant={'body2'} className={classes.score}>
+                <Typography variant={'body2'} sx={style.score}>
                   {ea.score}
                 </Typography>
-              </div>
+              </Box>
             );
           })}
-        </div>
+        </Box>
       ))}
     </CardContent>
   );
 }
 
-export default RecordTable;
+const style = {
+  content: {
+    padding: '0 32px 32px 32px',
+  },
+  eventContainer: {
+    marginBottom: '8px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    cursor: 'pointer',
+  },
+  score: {
+    backgroundColor: 'secondary.main',
+    padding: '2px 4px',
+    borderRadius: '6px',
+    margin: '2px 0',
+  },
+};
