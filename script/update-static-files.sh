81476200cbd46005fba07e610207bfadf36b25c3
FredLavoie <flavoie3@gmail.com>
2022-05-03 21:45:42 -0400

    Update build script to have safeguard against failed builds

--- script/update-static-files.sh
+++ script/update-static-files.sh
@@ -7,14 +7,23 @@ cd ~/workout-tracker/client;
 yarn build;
 wait
 
-echo "Copying static files into nginx static folder"
-sudo cp ~/workout-tracker/client/build/index.html /var/www/workout/index.html;
-sudo mv /var/www/workout/static  /var/www/workout/static-old;
-sudo cp -r ~/workout-tracker/client/build/static /var/www/workout/static;
-sudo cp -r /var/www/workout/static-old/admin /var/www/workout/static/admin;
+if [[ ! -d "~/workout-tracker/client/build" ]]; then
+  echo "build process failed. ending script.";
+	return;
+fi
 
+echo "Copying index.html file into nginx static folder"
+sudo cp ~/workout-tracker/client/build/index.html /var/www/workout/index.html;
+echo "Backing up static files to backup directory"
+sudo mv /var/www/workout/static  /var/www/workout/static-old;
+echo "Copying static build files into nginx folder"
+sudo cp -r ~/workout-tracker/client/build/static /var/www/workout/static;
+echo "Copy static admin files back into nginx static folder"
+sudo cp -r /var/www/workout/static-old/admin /var/www/workout/static/admin;
 echo "Removing old static files"
 sudo rm -r /var/www/workout/static-old;
+echo "Removing build directory from repo"
+sudo rm -r ~/workout-tracker/client/build;
 
 echo "Restarting nginx"
 cd $CWD;
