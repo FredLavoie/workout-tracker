26e5a3e9d0fdde60592a86c3cae6f7fa0b69ade0
FredLavoie <flavoie3@gmail.com>
2022-05-08 18:40:28 -0400

    Starting code for react components testing

---
+++ client/src/components/__tests__/CalendarGrid.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import { CalendarGrid } from '../CalendarGrid';
+
+afterEach(cleanup);
+
+describe('CalendarGrid', () => {
+  const year = '2022';
+  const month = '05';
+  const workouts =
+    [
+      { id: 123, date: '2022-05-04', workout_body: 'Workout1' },
+      { id: 456, date: '2022-05-12', workout_body: 'Workout2' },
+      { id: 789, date: '2022-05-28', workout_body: 'Workout3' },
+    ];
+
+
+  it('renders calendarGrid without crashing', () => {
+    render(<CalendarGrid year={year} month={month} workouts={workouts} />);
+  });
+
+  it('renders the calendarGrid with 31 days and the workout IDs assigned to the day id', () => {
+    render(<CalendarGrid year={year} month={month} workouts={workouts} />);
+    // confirm that there are 31 calendar days rendered
+    const calendarDays = screen.getAllByTestId('calendar-day');
+    expect(calendarDays).toHaveLength(31);
+    // confirm that the day with a specific workout has the workout id as its id
+    const firstWorkoutDayId = screen.getByText('28').getAttribute('id');
+    expect(firstWorkoutDayId).toBe('789');
+  });
+});
---
+++ client/src/components/__tests__/RecordTable.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { RecordTable } from '../RecordTable';
+
+afterEach(cleanup);
+
+describe('RecordTable', () => {
+  const type = 'strength';
+  const records =
+    [
+      { id: 456789, date: '2020-12-22', type: 'strength', event: 'Back Squat', score: 250 },
+      { id: 456756, date: '2021-02-30', type: 'strength', event: 'Back Squat', score: 275 },
+      { id: 190229, date: '2022-08-14', type: 'strength', event: 'Back Squat', score: 300 },
+    ];
+
+
+  it('renders RecordTable without crashing', () => {
+    render(<RecordTable type={type} records={records} />);
+  });
+});
