de613a35b5e74c89b89c3050d1ab36cdf9e4ea4b
FredLavoie <flavoie3@gmail.com>
2022-05-17 23:46:30 -0400

    Add package to mock API calls
    - install msw package
    - add catch-all mock routes for login
    - add mocking for localStorage

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import "./mockServer";
+
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
