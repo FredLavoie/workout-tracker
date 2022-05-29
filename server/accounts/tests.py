514bcc2d929c21f5d399f7167c39c6375e00ea66
FredLavoie <flavoie3@gmail.com>
2022-05-29 07:47:47 -0400

    Add a couple tests for the user creation

--- server/accounts/tests.py
+++ server/accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        new_user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpass'
+        )
+        self.assertEqual(new_user.username, 'testuser')
+        self.assertEqual(new_user.email, 'test@email.com')
+        self.assertTrue(new_user.is_active)
+        self.assertFalse(new_user.is_staff)
+        self.assertFalse(new_user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
