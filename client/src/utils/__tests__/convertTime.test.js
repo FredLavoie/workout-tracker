dfd8b003fd4b3fd8c8268aaff59141534d33ee93
Fred Lavoie <flavoie3@gmail.com>
2023-01-05 10:42:51 -0500

    Fix minor issue with 24 hours clock format in new workout form

--- client/src/utils/__tests__/convertTime.test.js
+++ client/src/utils/__tests__/convertTime.test.js
@@ -2,27 +2,35 @@ import { convertTime } from "../index";
 
 describe("convertTime", () => {
     it("[1] Get hour with minutes set to 15", () => {
-        const result = convertTime(["13", "13"]);
-        expect(result).toEqual("13:15");
+        const result = convertTime(["3", "13"]);
+        expect(result).toEqual("03:15");
     });
     it("[2] Get hour with minutes set to 30", () => {
-        const result = convertTime(["13", "15"]);
-        expect(result).toEqual("13:30");
+        const result = convertTime(["3", "15"]);
+        expect(result).toEqual("03:30");
     });
     it("[3] Get hour with minutes set to 30", () => {
-        const result = convertTime(["13", "28"]);
-        expect(result).toEqual("13:30");
+        const result = convertTime(["3", "28"]);
+        expect(result).toEqual("03:30");
     });
     it("[4] Get hour with minutes set to 45", () => {
-        const result = convertTime(["13", "43"]);
-        expect(result).toEqual("13:45");
+        const result = convertTime(["3", "43"]);
+        expect(result).toEqual("03:45");
     });
     it("[5] Get hour with minutes set to 00", () => {
-        const result = convertTime(["13", "45"]);
-        expect(result).toEqual("14:00");
+        const result = convertTime(["3", "45"]);
+        expect(result).toEqual("04:00");
     });
     it("[6] Get hour with minutes set to 00", () => {
-        const result = convertTime(["13", "46"]);
-        expect(result).toEqual("14:00");
+        const result = convertTime(["3", "46"]);
+        expect(result).toEqual("04:00");
+    });
+    it("[7] Confirm that next hour is 24 hour format", () => {
+        const result = convertTime(["12", "46"]);
+        expect(result).toEqual("13:00");
+    });
+    it("[8] Confirm that if it's currently 23:45 or later, that the current hour is returned instead of the next day", () => {
+        const result = convertTime(["23", "56"]);
+        expect(result).toEqual("23:45");
     });
 });
--- client/src/utils/convertTime.ts
+++ client/src/utils/convertTime.ts
@@ -1,6 +1,7 @@
 /**
  * Take a time array and return the time to the closest 15 min interval, rounding up.
  * 
+ * Example: ["9", "13"] => "09:15"
  * Example: ["13", "13"] => "13:15"
  * Example: ["13", "16"] => "13:30"
  * Example: ["13", "35"] => "13:45"
@@ -10,11 +11,13 @@
  * @return time in 24h format
  */
 export function convertTime(timeArr: string[]): string {
-    const hour = Number(timeArr[0]);
     const minutes = Number(timeArr[1]);
+    const hour = timeArr[0].padStart(2, "0");
+    const nextHour = String(Number(timeArr[0]) + 1).padStart(2, "0");
 
     if (minutes < 15) return `${hour}:15`;
     else if (minutes < 30) return `${hour}:30`;
     else if (minutes < 45) return `${hour}:45`;
-    else return `${hour + 1}:00`;
+    else if (nextHour === "24") return "23:45";
+    else return `${nextHour}:00`;
 }
