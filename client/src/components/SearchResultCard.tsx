0dd31b0d464b4fa51d2df4c04e805161a922b2c3
Fred Lavoie <flavoie3@gmail.com>
2023-01-11 20:34:54 -0500

    Fix issues with search cards
    - add the unique key that was missing in the processBodyText function
    - enable the highlighting of multi-word search queries

--- client/src/components/SearchResultCard.tsx
+++ client/src/components/SearchResultCard.tsx
@@ -24,6 +24,21 @@ export function SearchResultCard({ content, searchQuery }) {
     }
 
     /**
+     * Given the sub string under consideration, the function looks to see if the sub string contains
+     * the search query, and if so, returns true.
+     * If the search query is multiple words long, the function will return true if the search query
+     * array contains the sub string.
+     * 
+     * @param subString 
+     * @param searchQuery 
+     * @param searchQueryArr 
+     * @returns boolean
+     */
+    function queryInSubSubString(subString: string, searchQuery: string, searchQueryArr: string[]): boolean {
+        return subString.includes(searchQuery) || !!searchQueryArr.find((ea) => ea === subString);
+    }
+
+    /**
      * Process the workout body and highlight the search term in the returned
      * JSX element
      *
@@ -33,16 +48,19 @@ export function SearchResultCard({ content, searchQuery }) {
     function processBodyText(text: string) {
         const textArr = text.split("\n");
         const lowerCaseSearchQuery = searchQuery.toLowerCase();
+        const lowerCaseSearchQueryArr = searchQuery.toLowerCase().split(" ");
+        console.log("*** lowerCaseSearchQueryArr: ", lowerCaseSearchQueryArr);
+
         return (
             <Typography component="pre" sx={style.bodyText}>
                 {textArr.map((subString: string, index: number) => {
                     const lowerCaseSubString = subString.toLowerCase();
-                    return lowerCaseSubString.includes(lowerCaseSearchQuery) ?
+                    return lowerCaseSubString.includes(lowerCaseSearchQuery) && lowerCaseSearchQuery !== "" ?
                         (<div key={`${subString}-${index}`}>
                             <span>
                                 {subString.split(" ").map((subSubString: string, index2: number) => {
                                     const lowerCaseSubSubString = subSubString.toLowerCase();
-                                    return lowerCaseSubSubString.includes(lowerCaseSearchQuery) ?
+                                    return queryInSubSubString(lowerCaseSubSubString, lowerCaseSearchQuery, lowerCaseSearchQueryArr) ?
                                         (<span key={`${lowerCaseSubSubString}-${index2}`}><mark>{subSubString}</mark> </span>)
                                         :
                                         (<span key={`${lowerCaseSubSubString}-${index2}`}>{subSubString} </span>);
@@ -51,7 +69,7 @@ export function SearchResultCard({ content, searchQuery }) {
                             <br />
                         </div>)
                         :
-                        (<><span>{subString}</span><br /></>);
+                        (<><span key={`${subString}-${index}`}>{subString}</span><br /></>);
                 })}
             </Typography>
         );
