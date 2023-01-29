e7bbe6228decdbdef13c80812009ace99758b278
Fred Lavoie <flavoie3@gmail.com>
2023-01-29 13:03:27 -0500

    Update script - found issues + remove admin files backup

--- script/update-static-files.sh
+++ script/update-static-files.sh
@@ -1,16 +1,20 @@
 # This script updates the front end static files to be served by nginx.
 # The static files are to be generated locally and pushed to github.
 
+# Note that the admin files need to be updated manually if updates
+# are made to the admin files.
+
 echo "Copying index.html file into nginx folder"
 sudo cp ~/workout-tracker/client/build/index.html /var/www/workout/index.html;
-echo "Backing up asset files to backup directory"
-sudo mv /var/www/workout/assets  /var/www/workout/assets-old;
+
+echo "Backing up asset files to temp directory"
+sudo mv /var/www/workout/assets  /var/www/workout/temp;
+
 echo "Copying asset build files into nginx folder"
 sudo cp -r ~/workout-tracker/client/build/assets /var/www/workout/assets;
-echo "Copy asset admin files back into nginx asset folder"
-sudo cp -r /var/www/workout/assets-old/admin /var/www/workout/assets/admin;
+
 echo "Removing old asset files"
-sudo rm -r /var/www/workout/assets-old;
+sudo rm -r /var/www/workout/temp;
 
 echo "Restarting nginx"
 sudo systemctl restart nginx;
