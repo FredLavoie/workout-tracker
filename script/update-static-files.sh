162302147150b19254b236a8056131aa0128a579
Fred Lavoie <flavoie3@gmail.com>
2023-01-29 12:31:49 -0500

    Update static file script

--- script/update-static-files.sh
+++ script/update-static-files.sh
@@ -1,16 +1,16 @@
 # This script updates the front end static files to be served by nginx.
 # The static files are to be generated locally and pushed to github.
 
-echo "Copying index.html file into nginx static folder"
+echo "Copying index.html file into nginx folder"
 sudo cp ~/workout-tracker/client/build/index.html /var/www/workout/index.html;
-echo "Backing up static files to backup directory"
-sudo mv /var/www/workout/static  /var/www/workout/static-old;
-echo "Copying static build files into nginx folder"
-sudo cp -r ~/workout-tracker/client/build/static /var/www/workout/static;
-echo "Copy static admin files back into nginx static folder"
-sudo cp -r /var/www/workout/static-old/admin /var/www/workout/static/admin;
-echo "Removing old static files"
-sudo rm -r /var/www/workout/static-old;
+echo "Backing up asset files to backup directory"
+sudo mv /var/www/workout/assets  /var/www/workout/assets-old;
+echo "Copying asset build files into nginx folder"
+sudo cp -r ~/workout-tracker/client/build/assets /var/www/workout/assets;
+echo "Copy asset admin files back into nginx asset folder"
+sudo cp -r /var/www/workout/assets-old/admin /var/www/workout/assets/admin;
+echo "Removing old asset files"
+sudo rm -r /var/www/workout/assets-old;
 
 echo "Restarting nginx"
 sudo systemctl restart nginx;
